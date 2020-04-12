eb4a5397937be46b6fc5a123c3fea08b01a6d57c
Diego Parra <dp@linkfire.com>
2020-04-11 21:01:32 -0300

    initial files

---
+++ get-status/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"regexp"
+	"net/http"
+	"io/ioutil"
+	"os"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var resp string
+
+func getResponse(site string)  string  {
+	resp, err := http.Get(site)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	bodyString := string(body)
+	re := regexp.MustCompile("green")
+	fmt.Println(re.FindString(bodyString))
+	return re.FindString(bodyString)
+}
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context) (string, error) {
+	//resp := "green"
+	if resp = getResponse(os.Getenv("ES_ENDPOINT")); resp == "green" {
+		fmt.Println("ES rock-graylog Cluster Status: ", resp)
+		} else if resp == "yellow" {
+		fmt.Println("WARNING: Es rock-graylog cluster is in yellow state")
+		} else {
+			fmt.Println("CRITICAL: ES rock-graylog cluster is in Red state")
+		}
+
+	return fmt.Sprintf(resp), nil
+	}
+
+func main() {
+	lambda.Start(Handler)
+}
